1e20c73b7003e2aa2a780a6fcd4864de45a4ae2e
enzo.villanueva <enzo.villanueva@pragma.com.co>
2023-06-13 17:05:20 -0500

    Fixes

--- src/core/user/infraestructure/services/restaurant.service.repository.ts
+++ src/core/user/infraestructure/services/restaurant.service.repository.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import UserServiceRepository from '../../domain/user.service.repository'
 
-const serviceUrl = process.env.NODE_ENV === 'dev' ?  'http://192.168.18.116:3001' : process.env.API_PLAZOLETA
+const serviceUrl = process.env.NODE_ENV === 'dev' ?  'http://192.168.18.116:3001/api/v1/restaurantemployee/create' : process.env.API_INSERT_EMPLOYEE_RESTAURANT
 
 export default class RestaurantServiceRepository implements UserServiceRepository {
     
     async addRestaurantEmployee (restaurantId: string, chefId: string) {
         try {
-            const response = await axios.post(`${serviceUrl}/api/v1/restaurantemployee/create`, {
+            const response = await axios.post(`${serviceUrl}`, {
                 restaurantId,
                 chefId
             })
